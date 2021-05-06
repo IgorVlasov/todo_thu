69c0d8d1ac579207a685c682c034693a3eb16f84
Igor Vlasov <>
2021-05-06 15:55:14 +0000

    добавили работу команды add

---
+++ config.py
@@ -0,0 +1,11 @@
+#https://api.telegram.org/bot1639661384:AAGVqAimLNPYcMG19re4_T66cspn2CRel8M/getUpdates
+admin_id = 1296769964
+BOT_TOKEN = "1639661384:AAGVqAimLNPYcMG19re4_T66cspn2CRel8M"
+
+HELP = """
+/help  - вывод списка команд
+/add   - добавление задачи
+/show  - показать все задачи
+/done  - задача выполнена
+"""
+todo = {}
